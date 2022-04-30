8f3760c45b17882dd83acf6832a334f531ddbcb8
hakobmkoyan771 <95763847+hakobmkoyan771@users.noreply.github.com>
2022-04-30 13:56:13 +0400

    Update test_homework.py

--- test_homework.py
+++ test_homework.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import pytest
-import GitRepo.Python.Homework1.Fibonacci as tst
+import TasksRepo.Python.Homework1.Fibonacci as tst
 
 def test_main():
     assert tst.main(5) == 5
